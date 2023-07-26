52f84513cfa05aa2bfe7092cb50f404e756c5b17
Holyolg <63580448+Holyolg@users.noreply.github.com>
2023-07-26 16:20:00 +0300

    Add database to weekly swiper

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 var heroSwiper = new Swiper('.hero__swiper', {
     centerdSlides: true, 
     loop: true,
@@ -18,3 +20,78 @@ var weeklySwiper = new Swiper('.weeklySwiper', {
 
 })
 
+//dataBase
+
+const itemDB = [
+  {
+  img: '/src/Rectangle3.png',
+  endTimer: '02h 09m 12s',
+  name: 'Sun-Glass',
+  price: '1.75'
+}, 
+{
+  img: '/src/Rectangle3.png',
+  endTimer: '02h 09m 12s',
+  name: 'Sun-Glass',
+  price: '1.75'
+},
+{
+  img: '/src/Rectangle3.png',
+  endTimer: '02h 09m 12s',
+  name: 'Sun-Glass',
+  price: '1.75'
+},
+{
+  img: '/src/Rectangle3.png',
+  endTimer: '02h 09m 12s',
+  name: 'Sun-Glass',
+  price: '1.75'
+},
+{
+  img: '/src/Rectangle3.png',
+  endTimer: '02h 09m 12s',
+  name: 'Sun-Glass',
+  price: '1.75'
+},
+{
+  img: '/src/Rectangle3.png',
+  endTimer: '02h 09m 12s',
+  name: 'Sun-Glass',
+  price: '1.75'
+}
+]
+
+    
+  const weeklySwiperItems = document.querySelector('#weeklySwiper')
+  const removeItemsFromDB = (parent) => {
+    weeklySwiperItems.innerHTML = '';
+  }
+
+  function addItemsFromDB(dataBase, parent) {
+    removeItemsFromDB(parent)
+    for (let i = 0; i < dataBase.length; i++) {
+    console.log(dataBase[i])
+      parent.innerHTML += `
+      <div class="swiper-slide">
+      <div class="slide-box">
+         <div class="img-weekly__wrapper"> 
+          <img src="${dataBase[i].img}" class="swiper-slide__img-weekly">
+          <div class="slide-box__timer">${dataBase[i].endTimer}
+          </div>
+      </div>
+          <div class="slide-box__description">
+              <div class="slide-box__name">${dataBase[i].name}</div>
+          <div class="slide-box__wrapper">
+          <div class="slide-box__price">${dataBase[i].price}</div>
+     
+          <button class="slide-box__button">PLACE BID</button>
+      </div>
+          </div>
+      </div>
+  </div>
+      `;}
+  }
+
+  addItemsFromDB(itemDB, weeklySwiperItems)
+
+});
