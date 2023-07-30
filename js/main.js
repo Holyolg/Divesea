dbff0f6e407d66f95df47be37d275139ac8103e8
Holyolg <63580448+Holyolg@users.noreply.github.com>
2023-07-30 12:46:42 +0300

    Add form function and new close button

--- js/main.js
+++ js/main.js
@@ -48,6 +48,88 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	];
 
+	// Классы для юзеров
+
+	class User {
+		constructor(img, name, nickname, parentSelector, ...classes) {
+			this.img = img;
+			this.name = name;
+			this.nickname = nickname;
+			this.classes = classes;
+			this.parentSelector = document.querySelector(parentSelector);
+
+		}
+
+		render() {
+			const element = document.createElement('div');
+			if (this.classes.length === 0) {
+				this.element = 'best-seller__user';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
+
+			element.innerHTML = `
+			<div class="user__wrapper">
+			  <div class="user__description">
+				<img
+				  src=${this.img}
+				  class="avatar user__avatar"
+				/>
+				<div class="user__info">
+				  <div class="name user__name">${this.name}</div>
+				  <div class="nickname user__nickname">${this.nickname}</div>
+				</div>
+			  </div>
+			  <button class="user__button button-white">
+				Follow
+			  </button>
+			</div>
+			`;
+			this.parentSelector.append(element);
+		}
+	}
+
+	new User (
+		'src/img/avatars/avatar.png',
+		'Alex Ca.',
+		'@Alexey',
+		'.best-seller__users'
+	).render();
+	new User (
+		'src/img/avatars/image(3).png',
+		'Juliya Sa.',
+		'@JuliyaS',
+		'.best-seller__users'
+	).render();
+	new User (
+		'src/img/avatars/image(4).png',
+		'Trevor',
+		'@Trevor',
+		'.best-seller__users'
+	).render();
+	new User (
+		'src/img/avatars/image(1).png',
+		'Ca',
+		'@Ca',
+		'.best-seller__users'
+	).render();
+	new User (
+		'src/img/avatars/image(2).png',
+		'Pu Ca.',
+		'@Pu',
+		'.best-seller__users'
+	).render();
+	new User (
+		'src/img/avatars/icon.png',
+		'Alex Ca.',
+		'@Alexey',
+		'.best-seller__users'
+	).render();
+
+
+	const users = document.querySelector('.best-seller-users');
+
 	// const deadline = '2023-09-28'
 
 	// function getZero(num) {
@@ -268,9 +350,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Modal
 
 	const modalTrigger = document.querySelectorAll('[data-modal-trigger'),
-		modalClose = document.querySelector('[data-close]'),
 		modal = document.querySelector('[data-modal');
-	console.log(modalTrigger);
 
 	function openModal() {
 		modal.classList.add('open');
@@ -279,8 +359,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	function closeModal() {
-		modal.classList.remove('open');
 		modal.classList.add('close');
+		modal.classList.remove('open');
 		document.body.style.overflow = '';
 
 	}
@@ -291,12 +371,102 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
-	modalClose.addEventListener('click', closeModal);
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal || e.target.getAttribute('data-close') == '') {
+			console.log(e.target);
 
-	document.addEventListener('click', (e) => {
-		if (e.target === modal) {
 			closeModal();
 		}
 	});
 
+
+	// Forms
+
+	const message = {
+		loading: 'src/img/loading/load.gif',
+		succses: 'Спасибо, скоро мы свяжемся с вами!',
+		failure: 'Что-то пошло не так...'
+	};
+
+	const forms = document.querySelectorAll('form');
+
+	forms.forEach((form) => {
+		postData(form);
+	});
+
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const statusMessage = document.createElement('img');
+			statusMessage.src = message.loading;
+			statusMessage.style.cssText = `
+			display: block;
+			margin: 0 auto;
+			padding: 10px
+			`;
+			form.insertAdjacentElement('afterend', statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open('POST', 'server.php');
+
+			request.setRequestHeader('Content-Type', 'aplication/json');
+			const formData = new FormData(form);
+
+			// Для json формата
+			const obj = {};
+			formData.forEach((value, key) => {
+				obj[key] = value;
+			});
+			const json = JSON.stringify(obj);
+
+			request.send(json);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					showThanksModal(message.succses);
+					form.reset();
+					statusMessage.remove();
+				} else {
+					showThanksModal(message.failure);
+				}
+			});
+
+		});
+	}
+
+	function showThanksModal(message) {
+		const previousModalDialog = document.querySelector('.modal__wrapper');
+
+		previousModalDialog.classList.add('close');
+		openModal();
+
+		const thanksModal = document.createElement('div');
+		thanksModal.classList.add('modal__wrapper');
+
+		thanksModal.innerHTML = `
+		<div class='modal__content'>
+		<div data-close class="close__button">
+		<div class="close__button-block"></div>
+		<div class="close__button-block"></div>
+	  </div>
+
+	  <p class="title">${message}</p>
+	  </div>
+	  `;
+
+		document.querySelector('.modal').append(thanksModal);
+
+		setTimeout(() => {
+			thanksModal.remove();
+			previousModalDialog.classList.add('show');
+			previousModalDialog.classList.remove('close');
+			closeModal();
+		}, 3000);
+	}
+
 });
+
+
+
+
